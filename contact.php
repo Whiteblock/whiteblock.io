0c6cb9dc9b343b6a90f48909953280496a0069f9
Vuong Nguyen <nptvuong2912@gmail.com>
2018-07-12 14:17:43 -0700

    Responsive video for 320px width devices

--- contact.php
+++ contact.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'Demo contact form <demo@domain.com>';
-$sendTo = 'Demo contact form <demo@domain.com>'; // Add Your Email
+$from = 'Contact form <vuong@whiteblock.io>';
+$sendTo = 'Contact form <vuong@whiteblock.io>';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
