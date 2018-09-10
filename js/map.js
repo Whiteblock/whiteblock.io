c98153298889d1844b07ba7aa700c17d4cb518a7
Vuong Nguyen <nptvuong2912@gmail.com>
2018-09-10 09:58:37 -0700

    Clean up JS code

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 function initMap() {
 
     var map = new google.maps.Map(document.getElementById('ieatmaps'), {
-      center: {lat: 34.0937458, lng: -118.3614978},
+      center: {lat: 34.163910, lng: -118.521399},
       zoom: 12,
       styles: [
         {
